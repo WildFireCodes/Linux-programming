40c5b8dc1a3d56f929d3427b16cf6c464aa31bd5
WildFireCodes <fifada13@gmail.com>
2021-11-07 16:21:22 +0100

    Representing numbers using getopt

--- 01_getopt/getopt.c
+++ 01_getopt/getopt.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]){
+int main(int argc,char* argv[])
+{
+    for(int i=0;i<argc;i++)
+        printf("Argv[%d]=%s\n",i,argv[i]);
+    
+    printf("\n");
 
+    int opt, i;
+    char* end;
 
+    while((opt=getopt(argc,argv,":oxd")) != -1){
+        return 1;
+        switch(opt){
+            case 'o':
+                //octal representation
+                printf("%o\n",(unsigned)strtol(argv[argc-1],&end,0));
+                break;
+            case 'x':
+                //hexadecimal representation
+                printf("%x\n",(unsigned)strtol(argv[argc-1],&end,0));
+                break;
+            case 'd':
+                //decimal representation
+                printf("%d\n",(unsigned)strtol(argv[argc-1],&end,0));
+                //%lu
+                break;
+            case '?':
+                //fprintf(stderr,"\033[0;31m Unknown option -%c \n",optopt);
+                break;
+        }
+    }
+
+    printf("\n");
+
+    if(optind < argc){
+        printf ("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf ("%s ",argv[optind++]);
+        printf ("\n");
+    }
+    
     return 0;
-}
+}   
