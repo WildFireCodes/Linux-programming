1c3f9b0a47186689f5ad1f307b26475bc92950d8
WildFireCodes <fifada13@gmail.com>
2021-12-05 01:17:49 +0100

    .

--- 04/main.c
+++ 04/main.c
@@ -22,96 +22,114 @@ int find_max(int argc, char* argv[]){
         
         i++;    
     }
-
+    printf("MAX: %d\n", max);
     return max;
 }
 
-record* read_text_field(int id, char* argv[], record* ptr){
+record* read_text_field(int id, char* argv[], int max, record* ptr){
     for(int i=0; i<strlen(argv[id]); i++)
         ptr->text[i] = argv[id][i];
+
+    if(strlen(argv[id]) < max){
+        for(int j=strlen(argv[id]); j<max; j++)
+            ptr->text[j] = ' ';
+    }
 
     return ptr;
 }
 
-int last_record_id(int fd, int line_len){
-    return lseek(fd, 0, SEEK_END) - line_len;
+int find_last_id(int fd, record* ptr){
+    int last_id;
+    lseek(fd, 0, SEEK_SET);
+    while(lseek(fd, 0, SEEK_CUR) < lseek(fd, 0, SEEK_END)){
+        read(fd, ptr, 2);
+        ptr->id = last_id;
+        printf("odczytane id to: %d poz %ld \n", ptr->id, lseek(fd, 0, SEEK_CUR));
+        read(fd, ptr, 1);
+        read(fd, ptr, ptr->len+1);
+    }
+    printf("Ostatnie ID to %d\n", last_id);
+    return last_id;
 }
 
 void write_to_db(int fd, int argc, int max, char* argv[], record* ptr){
-    if(ptr == NULL){
-        exit(EXIT_FAILURE);
-    }
+    static int id;
 
-    if(lseek(fd, 0, SEEK_END) == 0)
+    if(lseek(fd, 0, SEEK_END) == 0){
         ptr->id = 0;
-    
+        id = 0;
+    }
     else{
-        lseek(fd, last_record_id(fd, sizeof(record)+max+1), SEEK_SET);
-        read(fd, ptr, 2);
-        lseek(fd, 0, SEEK_END);
+        int idid = find_last_id(fd, ptr);
     }
 
-    for(int i=2; i<argc; i++, ptr->id++){
-        printf("%d ustalone id\n", ptr->id);
-        read_text_field(i, argv, ptr);
+    for(int i=2; i<argc; i++){
+        ptr->id = id++;
+        read_text_field(i, argv, max, ptr);
         write(fd, ptr, sizeof(record)+max);
         write(fd, "\n", 1);
         memset(ptr->text, 0, max);
     }
-        
+}
 
-        lseek(fd, 0, SEEK_SET);
-        record* test = malloc(sizeof(record) + max);
-        read(fd, test, sizeof(record)+max);
-        printf("%d id, %d to len, %s to odczytane \n", test->id, test->len, test->text);
-        lseek(fd, sizeof(record)+max+1, SEEK_SET);
-        read(fd, test, sizeof(record)+max);
-        printf("%d id, %d to len, %s to odczytane \n", test->id, test->len, test->text); 
+void read_db(int fd, record* ptr){
+    int end;
+    if((end=lseek(fd, 0, SEEK_END)) == 0){
+        perror("Empty file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    lseek(fd, 0, SEEK_SET);
+    while(lseek(fd, 0, SEEK_CUR) < end){
+        read(fd, ptr, 3);
+        lseek(fd, -3, SEEK_CUR);
+        read(fd, ptr, ptr->len+3);
+        printf("%d %d %s\n", ptr->id, ptr->len, ptr->text);
+        lseek(fd, 1, SEEK_CUR);
+    }
 }
 
 int main(int argc, char* argv[]){
     int fd;
 
+    int max = find_max(argc, argv);
 
-    /*switch(argc){
+    record* x = malloc(sizeof(record) + max);
+    if(x == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    x->len = max;
+
+    switch(argc){
         case(1):
         perror("No args passed!\n");
         exit(EXIT_FAILURE);
         break;
 
         case(2):
-        fd = open(argv[argc-1], O_RDWR);
-        //wyswietl zawartosc pliku
+        fd = open(argv[1], O_RDWR);
+        if(fd < 0)
+            exit(EXIT_FAILURE);
+
+        read_db(fd, x);
         break;
 
         default:
+        fd = open(argv[1], O_CREAT | O_RDWR | O_APPEND);
+        if(fd < 0)
+            exit(EXIT_FAILURE);
 
-        fd = open(argv[argc-1], O_CREAT, O_RDWR, O_APPEND);
+        write_to_db(fd, argc, max, argv, x);
         break;
-    }*/
-
-    fd = open(argv[1], O_RDWR | O_CREAT, S_IRWXU);
-    if(fd < 0){
-        perror("Error while opening/creating file!\n");
-        exit(EXIT_FAILURE);
     }
-
-    int max = find_max(argc, argv);
-    if(max == 0){
-        perror("Empty line.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    record* x = malloc(sizeof(record) + max);
-    x->len = max;
-
-    write_to_db(fd, argc, max, argv, x);
 
     if( close(fd) <0 ){
         perror("Error while closing file! \n");
         exit(EXIT_FAILURE);
     }
 
+    free(x);
 
     return 0;
 }
