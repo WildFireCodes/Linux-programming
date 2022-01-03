65f253f4e389ac3d90b1a7af68d5668c2647418b
WildFire <wildfire@pop-os.localdomain>
2022-01-03 21:03:16 +0100

    bubble_sort using execv finished

--- 07_fork_exec_processes/5_bubble_sort.c
+++ 07_fork_exec_processes/5_bubble_sort.c
@@ -11,66 +11,37 @@ int main(int argc, char** argv){
         exit(EXIT_FAILURE);
     }
 
-    //rosnaca kolejnosc
-    // 3 1 5 4
-    // 1 3 5 4
-    // 1 3 4 5 za pomoca exec
-
-    //argumenty przychodza jako chary* -> konwersja na float -> wyjscie jest charem*
-
-    //wypisanie argumentow wejsciowych
     printf("INPUT: \n");
+
     for(int i=1; i<argc; i++)
         printf("%s ", argv[i]);
+        
     printf("\n");
 
-    //tablica na konwersje na float
-    float* tab = (float*)malloc((argc-1) * sizeof(float));
-    if(!tab){
-        perror("Error while allocating memory for float tab.\n");
-        exit(EXIT_FAILURE);
-    } 
-
-    //tablica wyjsciowa
-    char** output = (char**)malloc((argc-1) * sizeof(char*));
-    if(!output){
-        perror("Error while allocating memory for output tab.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    //konwersja
-    for(int i=1; i<argc; i++)
-        tab[i-1] = atof(argv[i]);
-
-    boolean again = F;
+    boolean sort_again = F;
 
     for(int i=1; i<argc-1; i++){
-        if(tab[i-1] > tab[i]){
-            float help = tab[i];
-            tab[i] = tab[i-1];
-            tab[i-1] = help;
-            again = T;
+        if(*argv[i] > *argv[i+1]){
+            char* help = argv[i];
+            argv[i] = argv[i+1];
+            argv[i+1] = help;
+
+            sort_again = T;
             break;
         }
     }
 
-    printf("OUTPUT: \n");
-    char buffer[10];
-    for(int i=0; i<argc-1; i++){
-        sprintf(buffer, "%f", tab[i]);
-        //gcvt(tab[i], 10, buffer);
-        //output[i] = buffer;
-        strcpy(output[i], buffer); //tutaj chyba trzeba przesunac wskaznik ile sie zapisalo
-        printf("%s ", output[i]);
-    }
+    printf("OUTPUT: \n"); 
 
+    for(int i=1; i<argc; i++)
+        printf("%s ", argv[i]);
     printf("\n");
-    if(again == F)
-        exit(0);
-    printf("\n\n");
-    for(int i=0; i<argc-1; i++){
-        printf("%s ", output[i]); //jedynie ostatnia liczba jest przekazywana do ponownego wykonania
-    }
-    printf("Wykonuje program ponownie. \n");
-    execv("5_bubble_sort", output);
+
+    if(sort_again == T){
+        printf("Wykonuje program ponownie. \n\n");
+        execv("5_bubble_sort", argv);
+    } 
+    
+    printf("Tablica posortowana!\n\n");
+    exit(0);
 }
